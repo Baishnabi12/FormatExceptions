65cbf60dc5d9ef5757995e27ea207b979b7499c2
baish <baish@BAISHNABI>
2024-10-17 13:36:07 +0530

    Add project files.

---
+++ FormatExceptions/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        try
+        {
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("That's not a valid number");
+        }
+    }
+}
+
